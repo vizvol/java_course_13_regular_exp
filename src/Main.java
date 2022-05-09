ece5c90eb054655d15a4e7012a6c378eec3fb8ff
izvol <v.izvol@iteco-inno.ru>
2022-05-09 13:00:42 +0500

    Commit job

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder strB  = new StringBuilder("Я узнал, что у меня\n");
+        strB.append("И тропинка и лесок").append("\nРечка, небо голубое").append("\nЭто Родина моя,");
+        strB.append("\nВсех люблю на свете я!");
+        strB.insert(strB.indexOf("\n"), "\nЕсть огромная семья");
+        strB.insert(strB.indexOf("Речка"), "В поле каждый колосок\n");
+        strB.insert(strB.indexOf("Это Родина"), "Это все мое родное\n");
+        System.out.println(strB);
+
+
+        Pattern longWord = Pattern.compile("\\+*[а-яА-Я]{6,}");
+        Matcher matcher = longWord.matcher(strB.toString().toUpperCase());
+        System.out.println("\nВывод всех слов из стихотворения размером больше 5 букв:");
+        while (matcher.find())
+            System.out.println(matcher.group());
+    }
+
+
+
 }
